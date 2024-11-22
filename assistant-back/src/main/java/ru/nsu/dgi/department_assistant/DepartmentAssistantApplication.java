eb9c090cc66b1415aeeb0581b9bd1db4333d57f2
Artyom-Kitov <artem.k3790@gmail.com>
2024-11-22 16:12:54 +0700

    changed repo structure

---
+++ assistant-back/src/main/java/ru/nsu/dgi/department_assistant/DepartmentAssistantApplication.java
@@ -0,0 +1,12 @@
+package ru.nsu.dgi.department_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentAssistantApplication.class, args);
+    }
+}
---
+++ assistant-back/src/test/java/ru/nsu/dgi/department_assistant/DepartmentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.dgi.department_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
