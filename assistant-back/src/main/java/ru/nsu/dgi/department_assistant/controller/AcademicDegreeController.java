d9182ff7721ee458611319fee2f96e29c1483bc9
Mikhail Sartakov <m.sartakov@g.nsu.ru>
2025-03-14 01:11:40 +0700

    added api documentation for controllers that work with employee information

--- assistant-back/src/main/java/ru/nsu/dgi/department_assistant/controller/AcademicDegreeController.java
+++ assistant-back/src/main/java/ru/nsu/dgi/department_assistant/controller/AcademicDegreeController.java
@@ -1,5 +1,9 @@
 package ru.nsu.dgi.department_assistant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,14 +26,47 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/academic-degree")
+@Tag(
+        name = "Academic degrees",
+        description = "Provides basic operations for creating, updating, deleting and retrieving " +
+                "academic degree information."
+)
 public class AcademicDegreeController {
     private final AcademicDegreeService academicDegreeService;
 
+    @Operation(
+            summary = "Returns all academic degrees",
+            description = "Returns the list of academic degrees of all employees."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved"
+                    )
+            }
+    )
     @GetMapping()
     public ResponseEntity<List<AcademicDegreeResponseDto>> getAllAcademicDegrees() {
         return ResponseEntity.ok(academicDegreeService.getAll());
     }
 
+    @Operation(
+            summary = "Returns academic degree of an employee",
+            description = "Returns an academic degree of a certain employee by id of employee."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @GetMapping("/employee")
     public ResponseEntity<AcademicDegreeResponseDto> getAcademicDegreeByEmployeeId(
             @RequestParam("employeeId") UUID employeeId
@@ -37,6 +74,28 @@ public class AcademicDegreeController {
         return ResponseEntity.ok(academicDegreeService.getByEmployeeId(employeeId));
     }
 
+    @Operation(
+            summary = "Creates a new academic degree",
+            description =
+                    "Creates a new academic degree for an employee by employee id. Operation may not be possible " +
+                    "if an academic degree is already exists for this employee."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully created"
+                    ),
+                    @ApiResponse(
+                            responseCode = "412",
+                            description = "Entity already exists"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @PostMapping("/create")
     public ResponseEntity<AcademicDegreeResponseDto> createAcademicDegree(
             @RequestParam("employeeId") UUID employeeId,
@@ -47,6 +106,24 @@ public class AcademicDegreeController {
                 .body(academicDegreeService.create(employeeId, academicDegree));
     }
 
+    @Operation(
+            summary = "Updates an academic degree",
+            description =
+                    "Updates an academic degree for an employee by employee id. Does nothing if there's no " +
+                    "academic degree for chosen employee."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully updated"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @PutMapping("/update")
     public ResponseEntity<AcademicDegreeResponseDto> updateAcademicDegree(
             @RequestParam("employeeId") UUID employeeId,
@@ -55,6 +132,22 @@ public class AcademicDegreeController {
         return ResponseEntity.ok(academicDegreeService.update(employeeId, academicDegree));
     }
 
+    @Operation(
+            summary = "Deletes an academic degree",
+            description = "Deletes an academic degree of an employee by employee id. Returns no content response."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Successfully deleted"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteAcademicDegree(@RequestParam("employeeId") UUID employeeId) {
         academicDegreeService.deleteByEmployeeId(employeeId);
--- assistant-back/src/main/java/ru/nsu/dgi/department_assistant/controller/EmploymentStatusController.java
+++ assistant-back/src/main/java/ru/nsu/dgi/department_assistant/controller/EmploymentStatusController.java
@@ -1,5 +1,9 @@
 package ru.nsu.dgi.department_assistant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +24,48 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(
+        name = "Employment statuses",
+        description = "Provides basic operations for creating, updating, deleting and retrieving " +
+                "information about employment statuses."
+)
 @RequestMapping("/api/v1/employment-status")
 public class EmploymentStatusController {
     private final EmploymentStatusService employmentStatusService;
 
+    @Operation(
+            summary = "Returns all employment statuses",
+            description = "Returns all employment statuses according to employees that have employment record."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved"
+                    )
+            }
+    )
     @GetMapping()
     public ResponseEntity<List<EmploymentStatusResponseDto>> getEmploymentStatus() {
         return ResponseEntity.ok(employmentStatusService.getAll());
     }
 
+    @Operation(
+            summary = "Returns an employment status of an employee",
+            description = "Returns an employment status of a certain employee by employee id."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @GetMapping("/employee")
     public ResponseEntity<EmploymentStatusResponseDto> getByEmployeeId(
             @RequestParam("employeeId") UUID employeeId
@@ -36,6 +73,28 @@ public class EmploymentStatusController {
         return ResponseEntity.ok(employmentStatusService.getByEmployeeId(employeeId));
     }
 
+    @Operation(
+            summary = "Creates a new employment status",
+            description =
+                    "Creates a new employment status for an employee by employee id. " +
+                            "Operation may not be possible if an employment status is already exists for this employee."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully created"
+                    ),
+                    @ApiResponse(
+                            responseCode = "412",
+                            description = "Entity already exists"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @PostMapping("/create")
     public ResponseEntity<EmploymentStatusResponseDto> createEmploymentStatus(
             @RequestParam("employeeId") UUID employeeId,
@@ -46,6 +105,24 @@ public class EmploymentStatusController {
                 .body(employmentStatusService.create(employeeId, employmentStatusRequestDto));
     }
 
+    @Operation(
+            summary = "Updates an employment status",
+            description =
+                    "Updates an employment status for an employee by employee id. " +
+                            "Does nothing if there's no employment status for chosen employee."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully updated"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @PutMapping("/update")
     public ResponseEntity<EmploymentStatusResponseDto> updateEmploymentStatus(
             @RequestParam("employeeId") UUID employeeId,
@@ -54,6 +131,23 @@ public class EmploymentStatusController {
         return ResponseEntity.ok(employmentStatusService.update(employeeId, employmentStatusRequestDto));
     }
 
+    @Operation(
+            summary = "Deletes an employment status",
+            description = "Deletes an employment status of an employee by employee id. " +
+                    "Returns no content response."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Successfully deleted"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Entity not found"
+                    )
+            }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<EmploymentStatusResponseDto> deleteEmploymentStatus(
             @RequestParam("employeeId") UUID employeeId
