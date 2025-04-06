b06db99af978833d8ef69be11b4c82bc754877b0
Your Name <cominfyu@gmail.com>
2025-04-06 16:14:25 +0300

    вроде полностью готовый апи на заполнение доумента

--- assistant-back/src/main/java/ru/nsu/dgi/department_assistant/config/TemplateType.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.dgi.department_assistant.config;
-
-public enum TemplateType {
-    HTML,  // HTML-письмо
-    DOCX,  // DOCX-отчет
-    TXT     // PDF-документ (на будущее)
-}
--- assistant-back/src/main/java/ru/nsu/dgi/department_assistant/domain/repository/document/DocumentTemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.dgi.department_assistant.domain.repository.document;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nsu.dgi.department_assistant.domain.entity.document.DocumentTemplate;
-
-import java.util.UUID;
-
-public interface DocumentTemplateRepository extends JpaRepository<DocumentTemplate, UUID> {
-}
